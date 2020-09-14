c9147eaae5cc81d323f4f274493cf523968cc6cc
fayl001 <f.entica@yahoo.com>
2020-09-14 16:12:49 +0800

    updated links

--- index.php
+++ index.php
@@ -183,7 +183,7 @@
                           <img src="Images/encyclopedia.png">
                           <br/><br/><br/>
                           <p>This project was made from a book Encyclopedia of Animals. All the images here are filtered free to use and modified. Only I did was weld the application with clean intent :) .</p>
-                          <a href="https://felxgit.github.io/webproject2018/School%20Project/index.html"  target="_blank">Link here</a><br/>
+                          <a href="https://felxgit.github.io/encyclopedia"  target="_blank">Link here</a><br/>
                           <b><i>Date: </i></b>
                         </div>
                       <hr/>
@@ -192,7 +192,7 @@
                           <img src="Images/ency2.png">
                           <br/><br/><br/>
                           <p>The only possible reason for this is to test my ability without bootstrap.</p>
-                          <a href="https://felxgit.github.io/ency2.html"  target="_blank">Link here</a>
+                          <a href="https://felxgit.github.io/encyclopedia2"  target="_blank">Link here</a>
                         </div>
                         
                     </div>
